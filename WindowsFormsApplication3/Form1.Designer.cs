ad14425e5c954e46cfc915b3bdf9256bf6ca9e8f
Agata <221091@student.pwr.edu.pl>
2015-12-23 23:35:53 +0000

    agata

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Wylacz zasilanie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Wlacz zasilanie";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 65);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(59, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Enable";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 431);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Visible = false;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(196, 7);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(538, 498);
+            this.pictureBox.TabIndex = 23;
+            this.pictureBox.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication3.Properties.Resources.B;
+            this.pictureBox1.Location = new System.Drawing.Point(129, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(61, 99);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 457);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(128, 17);
+            this.checkBox2.TabIndex = 18;
+            this.checkBox2.Text = "Tryb obrotu manualny";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 27);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Obrot o 30 stopni";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 212);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 27);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Obrot o 90 stopni";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(13, 242);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(131, 17);
+            this.checkBox3.TabIndex = 21;
+            this.checkBox3.Text = "Przeciwna polaryzacja";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(111, 20);
+            this.textBox1.TabIndex = 22;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 2000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 573);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Stepper motor symulation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool zapadka = false;
+
+        int licznik = 0;
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                checkBox2.Checked = false;
+                checkBox2.Checked = true;
+            }
+
+            
+            if ((checkBox1.Checked == true) )
+            { MessageBox.Show("Tryb pracy", "Sterowanie silnika"); }
+            else
+            {
+               
+                MessageBox.Show("Tryb gotowosci", "Sterowanie silnika");
+                checkBox1.Checked = false;
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value == 13)
+            {
+                numericUpDown1.Value = 1;
+                licznik_met("minus");
+            }
+            if (numericUpDown1.Value == 0)
+            {
+                numericUpDown1.Value = 12;
+                licznik_met("minus");
+            }
+            show_frame((int)numericUpDown1.Value-1);
+            if (zapadka)
+                licznik_met("plus");
+            
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                if (checkBox2.Checked == true)
+                {
+                    numericUpDown1.Visible = true;
+                    numericUpDown1.Value = czas + 1;
+                    timer1.Stop();
+                    timer2.Stop();
+                    zapadka = true;
+                }
+                else
+                {
+                    czas = Convert.ToInt32(numericUpDown1.Value) - 1;
+                    numericUpDown1.Visible = false;
+                    zapadka = false;
+                }
+            }
+        }
+        int czas = 0;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+
+            timer1.Enabled = true;
+            timer1.Start();
+
+            MessageBox.Show("Obrot o 30 stopni", "Kat obrotu");
+
+
+        }
+
+        void show_frame(int n)
+        {
+            Bitmap[] bmps =
+            {
+                   global::WindowsFormsApplication3.Properties.Resources._1,
+                   global::WindowsFormsApplication3.Properties.Resources._2,
+                   global::WindowsFormsApplication3.Properties.Resources._3,
+                   global::WindowsFormsApplication3.Properties.Resources._4,
+                   global::WindowsFormsApplication3.Properties.Resources._5,
+                   global::WindowsFormsApplication3.Properties.Resources._6,
+                   global::WindowsFormsApplication3.Properties.Resources._7,
+                   global::WindowsFormsApplication3.Properties.Resources._8,
+                   global::WindowsFormsApplication3.Properties.Resources._9,
+                   global::WindowsFormsApplication3.Properties.Resources._10,
+                   global::WindowsFormsApplication3.Properties.Resources._11,
+                   global::WindowsFormsApplication3.Properties.Resources._12
+            };
+            pictureBox.Image = bmps[n];
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                if (checkBox3.Checked == false)
+                {
+                    czas++;
+                    licznik_met("plus");
+                    if (czas == 12)
+                    { czas = 0; }
+                    show_frame(czas);
+                }
+                else
+                {
+                    czas--;
+                    licznik_met("plus");
+                    if (czas == -1)
+                    {
+                        czas = 11;
+                    }
+                    show_frame(czas);
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                if (checkBox3.Checked == false)
+                {
+                    czas = czas + 3;
+                    licznik_met("plus");
+                    if (czas >= 12)
+                    { czas = 0; }
+                    show_frame(czas);
+                }
+                else
+                {
+                    czas = czas - 3;
+                    licznik_met("plus");
+                    if (czas <= -1)
+                    {
+                        czas = 11;
+                    }
+                    show_frame(czas);
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+         
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Enabled = true;
+            timer2.Start();
+
+            MessageBox.Show("Obrot o 90 stopni", "Kat obrotu");
+            //czas++;
+            //textBox1.Text = czas.ToString();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            show_frame(0);
+            
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+            
+            licznik = -1;
+            
+            if(timer1.Enabled)
+            {
+                timer1.Stop();
+                timer1.Start();
+            }
+            if (timer2.Enabled)
+            {
+                timer2.Stop();
+                timer2.Start();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = global::WindowsFormsApplication3.Properties.Resources.A;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = global::WindowsFormsApplication3.Properties.Resources.B;
+        }
+        
+        private void licznik_met(string znak)
+        {
+            switch (znak)
+            {
+                case "plus":
+                    licznik++;
+                    if (licznik == 12)
+                    {
+                        licznik = 0;
+                    }
+                    textBox1.Text = licznik.ToString();
+                    break;
+
+                case "minus":
+                    licznik--;
+                    if (licznik == -1)
+                    {
+                        licznik = 11;
+                    }
+                    textBox1.Text = licznik.ToString();
+                    break;
+            }
+        }
+    }
+}
